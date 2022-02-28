f626c2da1fe9791be178abd9e1cde5b5a91c7129
HKristof2130 <78855300+HKristof2130@users.noreply.github.com>
2022-02-28 23:19:34 +0100

    .

---
+++ 3.Gyakorlat/alapmatstat.R
@@ -0,0 +1,19 @@
+X <- c(0.63,0.58,6.44,4.68,2.35,3.33,3.77,1.23)
+avg <- average(X)
+
+sorted = sort(X)
+sorted
+
+
+
+
+
+average <- function(sorozat){
+  sum=0;
+  for(i in 1 : length(sorozat)){
+    sum= sum+ sorozat[i]
+  }
+  sum = sum/length(sorozat)
+  
+}
+
